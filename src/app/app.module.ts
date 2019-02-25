2b625f9da7f3c22d63d9258e80c40a0891a84133
Tim Lukens <tblukens87@gmail.com>
2019-02-24 20:08:07 -0600

    Sub routing, and cleanup. Parallax fix

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -16,6 +16,8 @@ import { ParallaxComponent } from './parallax/parallax.component';
 import { DropdownDirective } from './shared/dropdown.directive';
 import { ShoppingListService } from './shopping-list/shopping-list.service';
 import { AppRoutingModule } from './app-routing.module';
+import { RecipeStartComponent } from './recipes/recipe-start/recipe-start.component';
+import { RecipeEditComponent } from './recipes/recipe-edit/recipe-edit.component';
 
 @NgModule({
   declarations: [
@@ -29,7 +31,9 @@ import { AppRoutingModule } from './app-routing.module';
     ShoppingEditComponent,
     ParallaxDirective,
     ParallaxComponent,
-    DropdownDirective
+    DropdownDirective,
+    RecipeStartComponent,
+    RecipeEditComponent
   ],
   imports: [BrowserModule, FormsModule, HttpClientModule, AppRoutingModule],
   providers: [ShoppingListService],
